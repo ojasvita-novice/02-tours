77085ad3526391c9777a540224d7dcccdbc91a9d
ojasvita-novice <ojasvitawagisha@gmail.com>
2024-02-01 15:45:24 +0530

    first_commit

---
+++ setup/src/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="loadingio-spinner-bean-eater-iuz8bhpl74a"><div className="ldio-uza8qhz5h9k">
+<div><div></div><div></div><div></div></div><div><div></div><div></div><div></div></div>
+</div></div>
+
+    </div>
+  );
+};
+
+export default Loading;
